2ac4de9df6581f9353cce3b3d82fae00b4b4e0fe
Manohar-128 <bushettymanohar@gmail.com>
2022-12-15 14:12:23 +0530

    prog 61.py

---
+++ conversion of height.py
@@ -0,0 +1,9 @@
+a=height=float(input("enter your height:"))
+centermeters=height*30.48
+yard=height*0.333333
+inche=height*12
+miles=height*0.000189394
+print("centimeters of a person:",a*centermeters)
+print("yard of a person: ",a*yard)
+print("inche of a person:",a*inche)
+print("miles of a person:",a*miles)
